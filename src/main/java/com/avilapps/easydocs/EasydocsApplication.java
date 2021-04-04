1bb8264c7e694a816c50bd6deb13b3c9cfcac13f
José Escorche <josesco5@gmail.com>
2021-04-04 11:55:39 -0400

    Initial commit

---
+++ src/main/java/com/avilapps/easydocs/EasydocsApplication.java
@@ -0,0 +1,13 @@
+package com.avilapps.easydocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasydocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasydocsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/avilapps/easydocs/EasydocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avilapps.easydocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasydocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
