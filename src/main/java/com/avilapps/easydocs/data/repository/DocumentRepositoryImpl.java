af61707165c30a9f1016663f37b51828b98f1787
José Escorche <josesco5@gmail.com>
2021-05-01 16:26:56 -0400

    Add logic to update documents
    * Add mapper and update document repository

--- src/main/java/com/avilapps/easydocs/data/repository/DocumentRepositoryImpl.java
+++ src/main/java/com/avilapps/easydocs/data/repository/DocumentRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.avilapps.easydocs.data.repository;
 
 import com.avilapps.easydocs.data.repository.impl.DocumentCreateRepository;
+import com.avilapps.easydocs.data.repository.impl.DocumentUpdateRepository;
 import com.avilapps.easydocs.domain.model.Document;
 import com.avilapps.easydocs.domain.repository.DocumentRepository;
 import org.springframework.stereotype.Repository;
@@ -9,13 +10,21 @@ import org.springframework.stereotype.Repository;
 public class DocumentRepositoryImpl implements DocumentRepository {
 
     private final DocumentCreateRepository documentCreateRepository;
+    private final DocumentUpdateRepository documentUpdateRepository;
 
-    public DocumentRepositoryImpl(DocumentCreateRepository documentCreateRepository) {
+    public DocumentRepositoryImpl(DocumentCreateRepository documentCreateRepository, DocumentUpdateRepository documentUpdateRepository) {
         this.documentCreateRepository = documentCreateRepository;
+        this.documentUpdateRepository = documentUpdateRepository;
     }
 
     @Override
     public Document createDocument(Document document) {
         return documentCreateRepository.createDocument(document);
     }
+
+    @Override
+    public Document updateDocument(Document document) {
+        return documentUpdateRepository.updateDocument(document);
+    }
+
 }
